84141ff2282a9ec44e8fd984e597efcff75f78f9
dyllanwli <messsking@gmail.com>
2020-10-31 19:22:58 -0500

    update

--- python-cmr/collector-01.py
+++ python-cmr/collector-01.py
@@ -1,10 +1,12 @@
 from cmr import CollectionQuery, GranuleQuery
 import json
 
-api = GranuleQuery(mode="CMR_OPS")
+mode="CMR_OPS"
+
+api = GranuleQuery(mode=mode)
 # api = GranuleQuery(mode="CMR_UAT")
 
 collections = api.get_all()
 
-with open("CMR_UAT_collections.json", "w") as file:
+with open("{}_collections.json".format(mode), "w") as file:
     json.dump(collections, file)
