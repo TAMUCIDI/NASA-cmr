5fe7b251dc82caf13edb3da662dbd773a33e6b26
dyllanwli <messsking@gmail.com>
2020-11-10 00:08:38 -0600

    update collector

--- python-cmr/collector-01.py
+++ python-cmr/collector-01.py
@@ -6,7 +6,7 @@ mode="CMR_OPS"
 api = CollectionQuery(mode=mode)
 # api = GranuleQuery(mode="CMR_UAT")
 
-collections = api.format("dif10").get_all()
+collections = api.format("native").concept_id(["C1214614210-SCIOPS"]).get_all()
 
 # with open("{}_collections.json".format(mode), "w") as file:
 #     json.dump(collections, file)
